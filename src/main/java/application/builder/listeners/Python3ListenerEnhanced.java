a9f002f627cdec8ea782ed0c9767f84fb61e097a
xclwt <1260918796@qq.com>
2019-03-16 10:40:09 +0800

    Update Python3ListenerEnhanced.java

--- src/main/java/application/builder/listeners/Python3ListenerEnhanced.java
+++ src/main/java/application/builder/listeners/Python3ListenerEnhanced.java
@@ -11,42 +11,42 @@ import java.util.Stack;
 
 public class Python3ListenerEnhanced extends Python3BaseListener{
 	private Stack<Vertex> vertexStack;
-    private CDG cdg;
+	private CDG cdg;
 
-    private void addVertexWithCtxAndEdgeThenPushToStack(VertexType vertexType, ParserRuleContext ctx) {
-        Vertex newVertex = createVertexWithCtx(vertexType, ctx);
-        addVertexAndEdge(newVertex);
-        pushToStack(newVertex);
-    }
+	private void addVertexWithCtxAndEdgeThenPushToStack(VertexType vertexType, ParserRuleContext ctx) {
+		Vertex newVertex = createVertexWithCtx(vertexType, ctx);
+		addVertexAndEdge(newVertex);
+		pushToStack(newVertex);
+	}
 
-    private Vertex createVertexWithCtx(VertexType vertexType, ParserRuleContext ctx) {
-        Vertex newVertex = new Vertex(vertexType);
-        newVertex.setCtx(ctx);
-        return newVertex;
-    }
+	private Vertex createVertexWithCtx(VertexType vertexType, ParserRuleContext ctx) {
+		Vertex newVertex = new Vertex(vertexType);
+		newVertex.setCtx(ctx);
+		return newVertex;
+	}
 
-    private void addVertexAndEdge(Vertex newVertex) {
-        cdg.addVertex(newVertex);
-        cdg.addEdge(vertexStack.peek(), newVertex);
-    }
+	private void addVertexAndEdge(Vertex newVertex) {
+		cdg.addVertex(newVertex);
+		cdg.addEdge(vertexStack.peek(), newVertex);
+	}
 
-    private void pushToStack(Vertex vertex) {
-        vertexStack.push(vertex);
-    }
+	private void pushToStack(Vertex vertex) {
+		vertexStack.push(vertex);
+	}
 
-    /* */
+	/* */
 	
 	@override
 	public void enterFile_input(Python3Parser.File_inputContext ctx) {
 		Vertex headVertex = createVertexWithCtx(VertexType.HEAD, ctx);
-        cdg.addVertex(headVertex);
-        vertexStack.push(headVertex);
+		cdg.addVertex(headVertex);
+		vertexStack.push(headVertex);
 	}
 	
 	@Override
-    public void exitFile_input(Python3Parser.File_inputContext ctx) {
-        vertexStack.pop();
-    }
+	public void exitFile_input(Python3Parser.File_inputContext ctx) {
+		vertexStack.pop();
+	}
 	
 	@override
 	public void enterExpr_stmt(Python3Parser.Expr_stmtContext ctx) {
@@ -66,9 +66,7 @@ public class Python3ListenerEnhanced extends Python3BaseListener{
 	@Override
 	public void exitFuncdef(Python3Parser.FuncdefContext ctx) {
 		vertexStack.pop();
-	}
-	
-	
+	}	
 }
 
 
